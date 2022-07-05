1be4c632b712c6f8d02fb83b302263dc1eb2d406
shaghasemi <shaghasemi@gmail.com>
2022-07-05 12:13:55 +0430

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MySpriteTutorial extends FlameGame {
   late SpriteAnimationComponent robotRunning;
   late SpriteAnimationComponent robotJumping;
   late SpriteAnimationComponent girlWalking;
+  late SpriteComponent background;
 
   @override
   Future<void>? onLoad() async {
@@ -31,6 +32,10 @@ class MySpriteTutorial extends FlameGame {
       'girl_walk.png',
       'girl_walk.json',
     );
+    background = SpriteComponent()
+      ..sprite = await loadSprite('girl_bg.jpg')
+      ..size = size;
+    add(background);
     SpriteAnimation run = SpriteAnimation.spriteList(
       spriteSheetRun,
       stepTime: 0.2,
@@ -57,15 +62,15 @@ class MySpriteTutorial extends FlameGame {
 
     girlWalking = SpriteAnimationComponent()
       ..animation = walk
-      ..position = Vector2(40, 30)
-      ..size = Vector2(150, 150);
+      ..position = Vector2(40,size[1]-170)
+      ..size = Vector2(100, 100);
     add(girlWalking);
   }
 
   @override
   void update(double dt) {
     super.update(dt);
-    girlWalking.y += dt * 10;
+    // girlWalking.y += dt * 10;
     girlWalking.x += dt * 20;
   }
 }
